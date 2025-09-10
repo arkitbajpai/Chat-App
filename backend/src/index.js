9a38624bcec8ac522a052176dea8ea09c71470aa
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-09-10 21:53:48 +0530

    Refactor server initialization to use environment variables for port configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5001;
 app.use("/api/auth",authRoutes)
-app.listen(5001,()=>{
+app.listen(PORT,()=>{
     console.log('Server is running on port 5001');
 })
