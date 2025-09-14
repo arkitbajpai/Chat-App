5aaf3301c4746d719aecddd4f452940d3d9b8dbb
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-09-14 22:14:00 +0530

    Fix response status code for existing user in signup controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,7 +8,7 @@ export const signup= async(req,res)=>{
     }
     const existingUser= await User.find({email});
     if(existingUser){
-        return res.status(401).json({message:"User already exists"});
+        return res.status(400).json({message:"User already exists"});
     }
     const salt= await bcrypt.genSalt(10);
     const hashPassword= await bcrypt.hash(password,salt);
