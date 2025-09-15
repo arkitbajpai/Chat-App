1ab4d137cf5f9093bc9cea595173b8b794b4362c
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-09-15 21:53:47 +0530

    Refactor signup and login functions to improve error handling and response structure

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,9 @@ import {User} from "../models/user.model.js";
 export const signup= async(req,res)=>{
     const {email,fullName,password}= req.body;
    try{
+    if(!fullName||!email||!password){
+        return res.status(400).json({message:"All fields are required"});
+    }
     if(password.length<6){
         return res.status(400).json({message:"Password must be at least 6 characters long"});
     }
@@ -39,9 +42,41 @@ export const signup= async(req,res)=>{
         res.status(500).json({message:"Internal server error"});
    }
 }
-export const login= (req,res)=>{
-    res.send("This is login route");
+export const login= async(req,res)=>{
+    const {email,password}= req.body;
+    try{
+        const user= await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid email or password"});
+        }
+      const isPasswordCorrect= await bcrypt.comapre(password,user.password);
+      if(!isPasswordCorrect){
+        return res.status(400).json({message:"Invalid email or password"});
+      }
+        grenrateToken(user._id,res);
+        res.status(200).json({
+            message:"Login successful",
+            user:{  
+                _id:user._id,
+                email:user.email,
+                fullName:user.fullName,
+            }
+        });
+
+    }
+     catch(err){
+         res.status(500).json({message:"Internal server error"});
+    }
+
 }
 export const logout= (req,res)=>{
-    res.send("This is logout route");
+    try{
+        res.cookie("jwt","",{
+            maxAge:0,
+             });
+        res.status(200).json({message:"Logout successful"});
+    }
+    catch(err){
+        res.status(500).json({message:"Internal server error"});
+    }
 }
